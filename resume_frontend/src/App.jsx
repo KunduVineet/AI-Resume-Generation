7b2f01f381d1064170f725bb70dedae18adf10d4
VineetKundu <kunduvineet6@gmail.com>
2025-03-31 12:38:54 +0530

    Frontend Setup

---
+++ resume_frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-red-600">Hello, Tailwind v4!</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
